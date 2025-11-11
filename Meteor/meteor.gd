838cdfc73c0bd1e3023aaf1efafbba830cfb6e02
Turbash <negirawatdeepi@gmail.com>
2025-11-11 22:08:52 +0530

    Added an approachng meteor

---
+++ Meteor/meteor.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+@export var minSpeed: float = 10
+@export var maxSpeed: float= 20
+@export var maxRotationRate: float=20
+@export var minRotationRate: float=-20
+
+var speed: float=0
+var rotationRate: float=0
+
+func _ready() -> void:
+	speed=randf_range(minSpeed,maxSpeed)
+	rotationRate=randf_range(minRotationRate,maxRotationRate)
+	
+func _physics_process(delta: float) -> void:
+	rotation_degrees+=rotationRate*delta
+	position.y+=speed*delta
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
