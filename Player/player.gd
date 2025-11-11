7473ce5753b8b68be54f2719e653496bbb89c7e5
Turbash <negirawatdeepi@gmail.com>
2025-11-11 18:35:18 +0530

    Initial Commit

---
+++ Player/player.gd
@@ -0,0 +1,41 @@
+extends Area2D
+
+var plBullet = preload("res://Bullet/bullet.tscn")
+
+@onready var animated_sprite=$AnimatedSprite
+
+@export var speed: float = 100
+var vel:= Vector2(0,0)
+
+func _process(delta: float) -> void:
+	if vel.x<0:
+		animated_sprite.play("left")
+	elif vel.x>0:
+		animated_sprite.play("right")
+	else:
+		animated_sprite.play("straight")
+		
+	if Input.is_action_pressed("shoot"):
+		var bullet:= plBullet.instantiate()
+		bullet.position=position
+		get_tree().current_scene.add_child(bullet)
+
+func _physics_process(delta: float) -> void:
+	var dirVec:=Vector2(0,0)
+	
+	if Input.is_action_pressed("move_left"):
+		dirVec.x=-1
+	elif Input.is_action_pressed("move_right"):
+		dirVec.x=1
+	if Input.is_action_pressed("move_up"):
+		dirVec.y=-1
+	elif Input.is_action_pressed("move_down"):
+		dirVec.y=1
+	
+	vel=dirVec.normalized() * speed
+	position+= vel*delta
+	
+	var viewRect:= get_viewport_rect()
+	position.x=clamp(position.x,0,viewRect.size.x)
+	position.y=clamp(position.y,0,viewRect.size.y)
+	
