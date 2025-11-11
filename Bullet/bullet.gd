18f3172a6883fc6958fa0fbecd69250cb80ebc28
Turbash <negirawatdeepi@gmail.com>
2025-11-11 22:19:34 +0530

    Added damge by bullets

--- Bullet/bullet.gd
+++ Bullet/bullet.gd
@@ -8,3 +8,9 @@ func _physics_process(delta: float) -> void:
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("damageable"):
+		area.damage(1)
+		queue_free()
--- Meteor/meteor.gd
+++ Meteor/meteor.gd
@@ -5,6 +5,8 @@ extends Area2D
 @export var maxRotationRate: float=20
 @export var minRotationRate: float=-20
 
+@export var life:int =20
+
 var speed: float=0
 var rotationRate: float=0
 
@@ -19,3 +21,8 @@ func _physics_process(delta: float) -> void:
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
+	
+func damage(amount: int):
+	life-=amount
+	if life<=0:
+		queue_free()
